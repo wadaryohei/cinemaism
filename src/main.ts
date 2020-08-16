82f832aa4e32853670a7da31bcca36905dc263cb
wadaryohei <ryohei.w0317wr@icloud.com>
2020-08-16 23:11:32 +0900

    自作の型定義ファイルを特定ディレクトリから読み込むように調整

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,14 @@ import VueCompositionAPI from '@vue/composition-api'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios, { AxiosStatic } from 'axios'
+import axios from 'axios'
 import VueAnalytics from 'vue-analytics'
 import '@/model/_shared/fontAwesome.js'
 import '@/assets/sass/style.scss'
 
-declare module 'vue/types/vue' {
-  interface Vue {
-    $axios: AxiosStatic;
-  }
-}
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
+
 Vue.use(VueCompositionAPI)
 Vue.use(VueAnalytics, {
   id: process.env.VUE_APP_API_GOOGLE_ANALYTICS,

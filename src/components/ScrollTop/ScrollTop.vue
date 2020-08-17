aa864d1a8940dc1aea1ddab531b56fa2388e9820
wadaryohei <ryohei.w0317wr@icloud.com>
2020-08-17 09:34:05 +0900

    ページ遷移時のScrollTopを処理するコンポーネントを追加

---
+++ src/components/ScrollTop/ScrollTop.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent, onMounted } from '@vue/composition-api'
+
+// ================================
+// Component
+// ================================
+export default defineComponent({
+  name: 'ScrollTop',
+  setup () {
+    onMounted(() => {
+      window.scrollTo(0, 0)
+    })
+  }
+})
+</script>
